b4512c3aad7f341f44cc69779ba8452a09d2abc7
Angel <angel777da@gmail.com>
2020-03-06 21:20:11 +0300

    fix android files save (utf-8)

--- Extentions.py
+++ Extentions.py
@@ -2,27 +2,27 @@
 
 import os
 
-_EXCLUDED = u':/?|;.<>*"'
+_EXCLUDED = ':/?|;.<>*"'
 
 
 def _trackSimpleData(track):
-	artist = u"".join([c for c in track.artists[0].name if c not in _EXCLUDED]) if track.artists else ""
-	album = u"".join([c for c in track.albums[0].title if c not in _EXCLUDED]) if track.albums else ""
+	artist = "".join([c for c in track.artists[0].name if c not in _EXCLUDED]) if track.artists else ""
+	album = "".join([c for c in track.albums[0].title if c not in _EXCLUDED]) if track.albums else ""
 	title = track.title
-	return title, album, artist
+	return title.encode("utf-8"), album.encode("utf-8"), artist.encode("utf-8")
 
 
 def folder(track):
 	title, album, artist = _trackSimpleData(track)
 
 	if album:
-		return u"%s/%s" % (artist, album)
+		return "%s/%s" % (artist, album)
 	return artist
 
 
 def filename(track):
-	title = u"".join([u"_" if c in _EXCLUDED else c for c in track.title])
-	return u"%s.mp3" % title
+	title = "".join(["_" if c in _EXCLUDED else c for c in track.title])
+	return ("%s.mp3" % title).encode("utf-8")
 
 
 def getTrackPath(prefixPath, track):
@@ -42,22 +42,22 @@ def checkFolder(path):
 
 def getArtistCover(artist):
 	if artist.cover:
-		return artist.cover.download, u"artist_%s.jpg" % artist.id
+		return artist.cover.download, "artist_%s.jpg" % artist.id
 
 
 def getAlbumCover(album):
 	if album.cover_uri:
-		return album.download_cover, u"album_%s.jpg" % album.id
+		return album.download_cover, "album_%s.jpg" % album.id
 	return getArtistCover(album.artists[0])
 
 
 def getTrackCover(track):
 	if track.cover_uri:
-		return track.download_cover, u"track_%s.jpg" % track.trackId
+		return track.download_cover, "track_%s.jpg" % track.trackId
 	return getAlbumCover(track.albums[0])
 
 
 def getPlaylistCover(playlist):
 	if playlist.cover:
-		return playlist.cover.download, u"playlist_%s_%s.jpg" % (playlist.playlistId, playlist.uid)
+		return playlist.cover.download, "playlist_%s_%s.jpg" % (playlist.playlistId, playlist.uid)
 	return None
